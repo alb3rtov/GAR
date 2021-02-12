ae577b2ef58852b39411a1a47e1a9fb267404aed
Alberto Vázquez <albertovmrm@gmail.com>
2021-02-12 12:57:18 +0100

    OID store and query next one

--- ejercicio11.sh
+++ ejercicio11.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [[ "$#" -ne 3 ]]
+then
+	echo "usage: $0 <domain name> <community> <SNMP version>"
+else
+	output=$(snmpget $3 -c $2 $1 .1.3.6.1.2.1.4.1.0)	
+	echo $output
+	oid=$(echo "$output" | cut -d " " -f1)
+	snmpgetnext $3 -c $2 $1 $oid
 
-
+fi
