ac82a925803b0945989129494c1015f46008cd25
Alberto Vázquez <albertovmrm@gmail.com>
2021-02-12 15:07:46 +0100

    Script snmpgetwalk working

--- ejercicio11.sh
+++ ejercicio11.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-if [[ "$#" -ne 3 ]]
+if [[ "$#" -ne 4 ]]
 then
-	echo "usage: $0 <domain name> <community> <SNMP version>"
-else
-	output=$(snmpget $3 -c $2 $1 .1.3.6.1.2.1.4.1.0)	
-	echo $output
-	oid=$(echo "$output" | cut -d " " -f1)
-	snmpgetnext $3 -c $2 $1 $oid
+	echo "usage: $0 <domain name> <community> <SNMP version> <OID>"
+else	
+	output=$(snmpget $3 -c $2 $1 -On $4.1.0 2> /dev/null)
 
+	if [[ "$?" -ne 0 ]]
+	then
+		echo "Error with de OID"
+		exit 1
+	else
+		snmpget $3 -c $2 $1 $4.1.0
+		oid=$(echo "$output" | cut -d " " -f1)
+		
+		while true;
+		do
+			output=$(snmpgetnext $3 -c $2 $1 -On $oid 2> /dev/null)	
+			oid=$(echo "$output" | cut -d " " -f1)
+
+			if [[ "$?" -ne 0 ]] || [[ $oid != *$4* ]]
+			then
+				break
+			fi
+			
+			snmpget $3 -c $2 $1 $oid
+		done
+		exit 0
+	fi
 fi
