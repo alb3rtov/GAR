063949bf96cafbdf2d83a4eca768af2b3b5903d5
Alberto Vázquez <albertovmrm@gmail.com>
2021-02-16 11:13:29 +0100

    Parameters fix

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -4,9 +4,9 @@ if [[ "$#" -ne 3  ]]
 then
 	echo "usage: $0 <domain name> <community> <SNMP version>"
 else
-	ipAddr=$(host tales.esi.uclm.es | awk '{print $4}')
-	ipIndex=$(snmpget -v1 -c gar tales .1.3.6.1.2.1.4.20.1.2.$ipAddr | awk '{print $4}')
-	macAddr=$(snmpget -v1 -c gar tales .1.3.6.1.2.1.2.2.1.6.$ipIndex | awk '{print $4}')
+	ipAddr=$(host $1 | awk '{print $4}')
+	ipIndex=$(snmpget $3 -c $2 $1 .1.3.6.1.2.1.4.20.1.2.$ipAddr | awk '{print $4}')
+	macAddr=$(snmpget $3 -c $2 $1 .1.3.6.1.2.1.2.2.1.6.$ipIndex | awk '{print $4}')
 
 	echo "MAC Address: $macAddr"
 	echo "Logical Address: $ipAddr"
